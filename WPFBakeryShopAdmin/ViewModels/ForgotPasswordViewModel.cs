4b3fddb938e73e1ce5c75ecebaf4c60d99bb47cb
Nguyen Ngoc Minh Tu <minhtu1392000@gmail.com>
2022-05-26 09:15:37 +0700

    save fortgotpass UI

---
+++ WPFBakeryShopAdmin/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFBakeryShopAdmin.Utilities;
+
+namespace WPFBakeryShopAdmin.ViewModels
+{
+    public class ForgotPasswordViewModel :Screen
+    {
+        public async Task SendEmail()
+        {
+            RestClient client = new RestClient(RestConnection.AUTHENTICATE_BASE_CONNECTION_STRING);
+            var task = await RestConnection.ExecuteRequestAsync(client, Method.Post, "/reset-password/init", null, null);
+            int statusCode = (int)task.StatusCode;
+            if(statusCode == 200)
+            {
+
+            }
+        }
+    }
+}
